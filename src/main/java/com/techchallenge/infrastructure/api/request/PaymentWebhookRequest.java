a76c47afd31d990ae6f8497d527f971246719fb3
danielcorreaa <daniel.cor@outlook.com>
2025-09-18 10:31:37 -0300

    ajustes, correcoes

--- src/main/java/com/techchallenge/infrastructure/api/request/PaymentWebhookRequest.java
+++ src/main/java/com/techchallenge/infrastructure/api/request/PaymentWebhookRequest.java
@@ -1,5 +1,13 @@
 package com.techchallenge.infrastructure.api.request;
 
-public record PaymentWebhookRequest(String resource) {
+public record PaymentWebhookRequest(String resource,  String topic,  String action) {
 
+    @Override
+    public String toString() {
+        return "PaymentWebhookRequest{" +
+                "resource='" + resource + '\'' +
+                ", topic='" + topic + '\'' +
+                ", action='" + action + '\'' +
+                '}';
+    }
 }
